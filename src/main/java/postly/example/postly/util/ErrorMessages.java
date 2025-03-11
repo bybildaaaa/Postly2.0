fd06adc9a51d0558dfe59de1f0db7b9b3b8ce8fa
bybildaaaa <dashanovikova1234y@gmail.com>
2025-03-11 12:03:00 +0300

    исправлено замечание сонара(добавлен приватный конструктор в ErrorMessages)

--- src/main/java/postly/example/postly/util/ErrorMessages.java
+++ src/main/java/postly/example/postly/util/ErrorMessages.java
@@ -3,4 +3,9 @@ package postly.example.postly.util;
 public class ErrorMessages {
     public static final String USER_NOT_FOUND = "User not found";
     public static final String POST_NOT_FOUND = "Post not found";
+
+    // Приватный конструктор, чтобы предотвратить создание экземпляров класса
+    private ErrorMessages() {
+        throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
+    }
 }
